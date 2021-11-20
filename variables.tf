6132bd664e43f20729a40b913810ec69a8e727e3
briamorr <78086763+briamorr@users.noreply.github.com>
2021-11-19 22:13:28 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "username" {
+  description = "APIC Username"
+  type        = string
+}
+
+variable "password" {
+  description = "APIC Password"
+  type        = string
+}
+
+variable "apicurl" {
+  description = "APIC URL"
+  type        = string
+}
+

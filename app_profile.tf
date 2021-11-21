d1fa52b48b84bd2292e4b5a0acf1ff260ea3b810
briamorr <briamorr@cisco.com>
2021-11-20 22:30:14 -0500

    names

--- app_profile.tf
+++ app_profile.tf
@@ -1,7 +1,7 @@
 resource "aci_application_profile" "test_ap" {
   tenant_dn  = aci_tenant.tenants["tenant1"].id
   count = 5
-  name       = "Application Profile ${count.index}"
+  name       = "ap_${count.index}"
   annotation = "tag"
   description = "from terraform"
   name_alias = "ap_${count.index}"
